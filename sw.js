4732ea70fde593cf2b4e0063754bcd917b0a1b58
evgenchuk1 <evgenchuk1@gmail.com>
2026-05-23 16:39:56 +0300

    Restore original app + add barcode numbers from photos (16 products)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vegcount-v3';
+const CACHE = 'vegcount-v4';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
